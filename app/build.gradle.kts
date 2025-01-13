775a16c7f4c7bf7103b8d2b5381ccbad76241a80
mr-oybek <khaydaraliev@gmail.com>
2025-01-13 16:29:42 +0100

    fix: Compile SDK version updraded from 34 to 35.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ozodbek.recorderapp_jetpackcompose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ozodbek.recorderapp_jetpackcompose"
