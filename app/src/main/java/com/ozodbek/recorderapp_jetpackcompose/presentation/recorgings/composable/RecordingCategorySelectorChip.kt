ac7ef5f1c64cfdf631f948e5c21ae6462416c541
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2025-01-15 23:59:54 +0100

    Add Recorder screen composables

---
+++ app/src/main/java/com/ozodbek/recorderapp_jetpackcompose/presentation/recorgings/composable/RecordingCategorySelectorChip.kt
@@ -0,0 +1,69 @@
+package com.ozodbek.recorderapp_jetpackcompose.presentation.recorgings.composable
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.ozodbek.recorderapp_jetpackcompose.R
+
+@Composable
+fun RecordingCategoryChipSelectorChip(
+	isSelected: Boolean,
+	category: RecordingCategoryModel,
+	onClick: () -> Unit,
+	modifier: Modifier = Modifier,
+) {
+	val chipBackground = category.categoryColor.colorRes
+		?: MaterialTheme.colorScheme.surfaceContainer
+
+	val chipContentColor = contentColorFor(chipBackground)
+
+	FilterChip(
+		selected = isSelected,
+		onClick = onClick,
+		label = { Text(text = category.name) },
+		shape = MaterialTheme.shapes.medium,
+		leadingIcon = {
+			AnimatedVisibility(
+				visible = isSelected,
+			) {
+				Icon(
+					painter = category.categoryType.imageVector,
+					contentDescription = stringResource(
+						R.string.category_icon_category,
+						category.name
+					),
+					modifier = Modifier.sizeIn(maxHeight = 20.dp, maxWidth = 20.dp),
+				)
+			}
+		},
+		trailingIcon = {
+			AnimatedVisibility(
+				visible = category.hasCount,
+				enter = slideInHorizontally() + fadeIn(),
+				exit = slideOutHorizontally() + fadeOut()
+			) {
+				Text(text = "(${category.count})")
+			}
+		},
+		colors = FilterChipDefaults.filterChipColors(
+			selectedContainerColor = chipBackground,
+			selectedLabelColor = chipContentColor,
+			selectedLeadingIconColor = chipContentColor,
+			iconColor = chipContentColor
+		),
+		modifier = modifier,
+	)
+}
