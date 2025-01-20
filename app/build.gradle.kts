ed3ffb631411599ffdc3228e5ad3a02ce4b0c525
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2025-01-20 23:55:31 +0100

    Add media3-player implementation in data and domain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,13 @@ dependencies {
 
     //splash
     implementation(libs.androidx.core.splashscreen)
+
+    //media3
+    implementation(libs.androidx.media3.common)
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.media3.session)
+    implementation(libs.androidx.media3.transformer)
 }
 
 
---
+++ app/src/main/java/com/ozodbek/recorderapp_jetpackcompose/domain/player/exceptions/SetPlayerCommandNotFound.kt
@@ -0,0 +1,4 @@
+package com.ozodbek.recorderapp_jetpackcompose.domain.player.exceptions
+
+class SetPlayerCommandNotFound :
+	Exception("Cannot set player media item please configure it correclty")
